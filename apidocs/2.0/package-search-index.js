58a4f625e9733e72161812860e839da4a4808b7a
Niklas Schlimm <ns103@hotmail.de>
2019-10-10 23:10:07 +0200

    Deploy website
    Deploy website version based on 3e584cf48f7baf6328140daa1fee8e738ed4bde9

---
+++ apidocs/2.0/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.projectbarbel.histo"},{"l":"org.projectbarbel.histo.event"},{"l":"org.projectbarbel.histo.extension"},{"l":"org.projectbarbel.histo.functions"},{"l":"org.projectbarbel.histo.model"}]
